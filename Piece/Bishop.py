7d008c3f0b669c356bbdfa1979c6f92ce679d938
Anirudh Rudraraju <anirudh@Anirudhs-MacBook-Pro.local>
2019-11-22 20:53:11 -0800

    define allowed moves method for bishop class

--- Piece/Bishop.py
+++ Piece/Bishop.py
@@ -1,5 +1,42 @@
+from helper import *
 class Bishop:
     id = 'B'
     def __init__(self,alliance,coord):
         self.alliance = alliance
         self.coord = coord
+
+    def allowed_moves(self,board):
+        allowed_moves=[]
+        #diag1
+        square = ((self.coord[0]+1,self.coord[1]+1))
+        while coord_inside_board(square) and null_piece(board,square):  
+            allowed_moves.append(square)
+            square = (square[0]+1,square[1]+1)
+        else:
+            if coord_inside_board(square) and enemy_piece(board,square,self.alliance):
+                allowed_moves.append(square)
+        #diag2
+        square = ((self.coord[0]-1,self.coord[1]+1))
+        while coord_inside_board(square) and null_piece(board,square):  
+            allowed_moves.append(square)
+            square = (square[0]-1,square[1]+1)
+        else:
+            if coord_inside_board(square) and enemy_piece(board,square,self.alliance):
+                allowed_moves.append(square)
+        #diag3
+        square = ((self.coord[0]-1,self.coord[1]-1))
+        while coord_inside_board(square) and null_piece(board,square):  
+            allowed_moves.append(square)
+            square = (square[0]-1,square[1]-1)
+        else:
+            if coord_inside_board(square) and enemy_piece(board,square,self.alliance):
+                allowed_moves.append(square)
+        #diag4
+        square = ((self.coord[0]+1,self.coord[1]-1))
+        while coord_inside_board(square) and null_piece(board,square):  
+            allowed_moves.append(square)
+            square = (square[0]+1,square[1]-1)
+        else:
+            if coord_inside_board(square) and enemy_piece(board,square,self.alliance):
+                allowed_moves.append(square)
+        return allowed_moves
