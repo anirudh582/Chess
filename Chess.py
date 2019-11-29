e247878bf901ae7910bade78fef1c9ccfbea3bb1
Anirudh Rudraraju <anirudh@Anirudhs-MacBook-Pro.local>
2019-11-29 03:31:10 -0800

    fix flip orientation inter board bug

--- Chess.py
+++ Chess.py
@@ -111,32 +111,44 @@ while running:
                     screen.blit(img,(mouse_x-offset_x,mouse_y-offset_y))
 
             if move_accepted:
-                data = (player_alliance, prev_initial_square, final_square)
+                if settings.flip:
+                    data = (player_alliance, (7-prev_initial_square[0],prev_initial_square[1]), (7-final_square[0],final_square[1]))
+                else:
+                    data = (player_alliance, prev_initial_square, final_square)
                 s.send(pickle.dumps(data))
+                print('sent:', data)
                 move_accepted = False
                 
         else:
             data = pickle.loads(s.recv(2048))
-            player_alliance_recv, opp_init_sq, opp_final_sq = data
+            print('received:', data)
+            player_alliance_recv, opp_init_sq_temp, opp_final_sq_temp = data
+            if settings.flip:
+                opp_init_sq = (7-opp_init_sq_temp[0], opp_init_sq_temp[1])
+                opp_final_sq = (7-opp_final_sq_temp[0], opp_final_sq_temp[1])
+            else:
+                opp_init_sq = opp_init_sq_temp
+                opp_final_sq = opp_final_sq_temp
+            print(opp_init_sq,opp_final_sq)    
             moved_piece = new_board.board[opp_init_sq[1]][opp_init_sq[0]]
             new_board.board[opp_init_sq[1]][opp_init_sq[0]] = Null(opp_init_sq)
             new_board.board[opp_final_sq[1]][opp_final_sq[0]] = moved_piece
-            #if moved_piece.id == 'K':
-            #    new_board.update_king_position(coord,piece.alliance)
-            #    move_rook_if_castling(new_board,piece,coord)
-            #if moved_piece.id == 'R' or moved_piece.id == 'K':
-            #    new_board.board[opp_final_sq[1]][opp_final_sq[0]].set_moved()
-            #new_board.update_all_attacked_squares()
+            if moved_piece.id == 'K':
+                new_board.update_king_position(coord,piece.alliance)
+                move_rook_if_castling(new_board,piece,coord)
+            if moved_piece.id == 'R' or moved_piece.id == 'K':
+                new_board.board[opp_final_sq[1]][opp_final_sq[0]].set_moved()
+            new_board.update_all_attacked_squares()
             plot_canvas()
             plot_board(new_board)
             prev_initial_square = opp_init_sq
             final_square = opp_final_sq
-            #if new_board.king_in_check(turn):
-            #    coord = new_board.king[turn]
-            #    if flip:
-            #        draw_red_wireframe_circle((coord[0],7-coord[1]))
-            #    else:
-            #        draw_red_wireframe_circle(coord)
+            if new_board.king_in_check(turn):
+                coord = new_board.king[turn]
+                if flip:
+                    draw_red_wireframe_circle((coord[0],7-coord[1]))
+                else:
+                    draw_red_wireframe_circle(coord)
             if(data):
                 turn = 'B' if turn == 'W' else 'W'
     
