1e08460513a0b67caa0b162e80f3ef31784c0755
anirudh582 <anirudh582@gmail.com>
2019-12-07 21:08:17 +0000

    make server script a little smarter

--- server.py
+++ server.py
@@ -17,12 +17,23 @@ print("Waiting for a connection, Server Started")
 
 player_alliance=['W','B']
 conns = {} 
+currentPlayer = 0
 def threaded_client(conn, player):
-    conn.send(str.encode(player_alliance[player]))
-    if player not in conns:
-        conns[player_alliance[player]]=conn
+    conn.send(str.encode(player_alliance[player%2]))
+    conns[player_alliance[player%2]]=conn
+    global currentPlayer
+    if currentPlayer<2:
+        print("sending not ready")
+        for _,conn in conns.items():
+            conn.send(str.encode("not ready"))
+    else:
+        print("sending ready")
+        for _,conn in conns.items():
+            conn.send(str.encode("ready"))
+
     while True:
         try:
+            print("ready to receive data for {}".format(player_alliance[player]))
             data = pickle.loads(conn.recv(2048))
             print("Received: ", data)
             player_alliance_recv, init_sq, final_sq, _= data
@@ -45,16 +56,20 @@ def threaded_client(conn, player):
         except:
             break
 
-    print("Lost connection: ", player_alliance[player])
+    if len(conns)>0:
+        try:
+            for key in conns.keys():
+                del conns[key]
+        except:
+            pass
+    currentPlayer=0
+    print("Lost connection: ", player_alliance[player%2])
     conn.close()
         
 
-
-currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-    print(conns)
     start_new_thread(threaded_client, (conn, currentPlayer)) 
-
+   
     currentPlayer += 1
